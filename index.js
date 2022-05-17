1f27ca57d7287b60f90b1cf2a14b2ee14d1c7e9b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-17 11:34:20 +0600

    booking api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //1
 const express = require('express');
-const app = express() 
-const port = process.env.PORT || 5000 
+const app = express()
+const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //3
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 //4
 app.use(cors())
-app.use(express.json()) 
+app.use(express.json())
 
 //5 
 
@@ -22,6 +22,7 @@ async function run() {
         //7
         await client.connect();
         const serviceCollection = client.db("handy-man").collection("services");
+        const bookingCollection = client.db("handy-man").collection("bookings");
 
         //8 service load (Read)
         app.get('/service', async (req, res) => {
@@ -30,13 +31,29 @@ async function run() {
             res.send(services)
         })
 
-        // 
-        app.get('/service/:id' , async (req,res) => {
+        // get single service 
+        app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
+
+        // booking 
+        app.post('/booking', async (req, res) => {
+            const booking = req.body
+            const query = { serviceName: booking.serviceName, email: booking.email, date: booking.date }
+            const exists = await bookingCollection.findOne(query)
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            else {
+                const result = await bookingCollection.insertOne(booking)
+                return res.send({ success: true, result });
+            }
+        })
+
+
 
     } finally {
         //   await client.close();
